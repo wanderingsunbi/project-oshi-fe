52b159f712adc990b5081ca7a63c97892afcd799
wanderingsunbi <neoguysm@gmail.com>
2026-01-15 18:38:43 +0900

    pseudo code in login, signup.js

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,10 @@
 export default function Login () {
-  return <div>Login Page</div>;
+  return (
+  <>
+    <form onSubmit={handleSubmit}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button>로그인 하기</button>
+    </form>
+  </>
+  );
 }
--- pages/signup.js
+++ pages/signup.js
@@ -1,3 +1,10 @@
 export default function SignUp () {
-  return <div>SignUp Page</div>;
+  return (
+  <>
+    <form onSubmit={handleSubmit}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button>회원가입 하기</button>
+    </form>
+  </>
+  );
 }

43dc3b1c74200a5a2852b591a9a3bdfb27fd9d5e
wanderingsunbi <neoguysm@gmail.com>
2026-01-15 20:41:16 +0900

    각 페이지 타이틀 텍스트 추가

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,9 @@
 export default function Login () {
   return (
   <>
+    <Head>
+      <title>로그인 :: 최애의 포토</title>
+    </Head>
     <form onSubmit={handleSubmit}>
       <input value={value} onChange={(e) => setValue(e.target.value)} />
       <button>로그인 하기</button>
